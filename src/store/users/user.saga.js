3054f7480605c43e5fac7979d1d0b326459d94ac
ossmazon <sergio.mazon@hotmail.com>
2026-01-19 13:10:15 -0600

    Implemented local storage

--- src/store/users/user.saga.js
+++ src/store/users/user.saga.js
@@ -13,6 +13,14 @@ import { v4 as uuidv4 } from "uuid"
 
 function* onFetchUsers() {
     try {
+
+        const saved = localStorage.getItem("users")
+        
+        if (saved) {
+            yield put(fetchUsersSuccess(JSON.parse(saved)))
+            return
+        }
+
         const response = yield call(getUsersApi)
         yield put(fetchUsersSuccess(response.data))
     } catch (error) {
@@ -23,13 +31,13 @@ function* onFetchUsers() {
 function* handleCreateUser(action) {
     try {
         yield delay(500)
-        const newId = uuidv4().slice(0,5)
+        const newId = uuidv4().slice(0, 5)
 
         const newUser = {
             id: newId,
             ...action.payload
         }
-        console.log( "new User", newUser)
+        console.log("new User", newUser)
         yield put(createUserSuccess(newUser))
     } catch (error) {
         yield put(createUserFailure("Failed to create user"))
